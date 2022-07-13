82a6107cc7f76a4399e02a5295b14b04c0055a93
Bonboncy99 <thaonhien9991@gmail.com>
2022-07-13 14:04:42 +0700

    ss1_database

--- ss1_database/exercise/studentmanager.sql
+++ ss1_database/exercise/studentmanager.sql
@@ -1,28 +1,29 @@
 create database student_management; 
 use student_management;
-create table student(
-id int auto_increment primary key,
-name varchar(50),
-age int,
-country varchar(50)
+CREATE TABLE student (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    country VARCHAR(50)
 );
 
-create table class(
-id int auto_increment primary key,
-name varchar(50)
+CREATE TABLE class (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50)
 );
 
-create table teacher(
-id int  auto_increment primary key,
-name varchar(50),
-age int,
-country varchar(50)
+CREATE TABLE teacher (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    country VARCHAR(50)
 );
 
-insert into student (name, age, country) values ("Bon", 23, "VietNam"),("Ben",24,"VN");
-insert into class (name) values ("C0422G1"),("C0522G1");
-insert into teacher (name, age, country) values ("mom", 50, "BUBU"),("Dad",50,"BOBO");
+insert into student (name, age, country) 
+values ("Bon", 23, "VietNam"),("Ben",24,"VN");
 
-select * from student;
-select * from class;
-select * from teacher;
+insert into class (name)
+ values ("C0422G1"),("C0522G1");
+ 
+insert into teacher (name, age, country) 
+values ("mom", 50, "BUBU"),("Dad",50,"BOBO");
